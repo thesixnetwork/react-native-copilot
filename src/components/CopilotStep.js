7bc6949d69fdfe3e2ec1cd8f67f7e057b0be4146
Tanapat Sainak <fallen_things@hotmail.com>
2019-08-20 12:52:39 +0700

    return null when can not get step

--- src/components/CopilotStep.js
+++ src/components/CopilotStep.js
@@ -1,37 +1,40 @@
 // @flow
-import { Component, createElement } from 'react';
-import PropTypes from 'prop-types';
+import { Component, createElement } from "react";
+import PropTypes from "prop-types";
 
-import ConnectedCopilotStep from './ConnectedCopilotStep';
+import ConnectedCopilotStep from "./ConnectedCopilotStep";
 
-import type { CopilotContext } from '../types';
+import type { CopilotContext } from "../types";
+import _ from "lodash";
 
 type Props = {
   name: string,
   order: number, // eslint-disable-line react/no-unused-prop-types
-  text: string, // eslint-disable-line react/no-unused-prop-types
+  text: string // eslint-disable-line react/no-unused-prop-types
 };
 
 class CopilotStep extends Component<Props> {
   static contextTypes = {
-    _copilot: PropTypes.object,
-  }
+    _copilot: PropTypes.object
+  };
 
   context: {
-    _copilot: CopilotContext,
-  }
+    _copilot: CopilotContext
+  };
 
   render() {
-    const currentStep = this.context._copilot.getCurrentStep();
-
-    return createElement(
-      ConnectedCopilotStep,
-      {
+    const currentStep = _.get(this, "context._copilot.getCurrentStep", () => {
+      return false;
+    })();
+    if (!!currentStep) {
+      return createElement(ConnectedCopilotStep, {
         ...this.props,
         _copilot: this.context._copilot,
-        visible: currentStep && currentStep.name === this.props.name,
-      },
-    );
+        visible: currentStep && currentStep.name === this.props.name
+      });
+    } else {
+      return null;
+    }
   }
 }
 
